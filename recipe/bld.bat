2c94d9db105a2ed14c522ff5d98b03bd36d8c2e4
Geoffrey Garrett <g.h.garrett13@gmail.com>
2020-08-04 00:10:53 +0200

    Attempt at windows build.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,7 +4,7 @@ if errorlevel 1 exit 1
 cd build
 if errorlevel 1 exit 1
 cmake ^
-    -G "Visual Studio 15 2017 Win64" ^
+    -G "MinGW Makefiles" ^
     -DCMAKE_CXX_STANDARD=17 ^
     -DBoost_NO_BOOST_CMAKE=ON ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
