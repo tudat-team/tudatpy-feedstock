9bf6801afc5f9960027fa2f423be169e1a33eadb
Geoffrey Hyde Garrett <g.h.garrett13@gmail.com>
2020-08-04 09:47:36 +0200

    update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,6 +6,7 @@ if errorlevel 1 exit 1
 cmake ^
     -G "MinGW Makefiles" ^
     -DCMAKE_CXX_STANDARD=11 ^
+    -D_hypot=hypot ^
     -DBoost_NO_BOOST_CMAKE=ON ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
