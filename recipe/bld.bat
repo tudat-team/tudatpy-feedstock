2f65b8d3a14bf8bec5cc3a97f55c2bbdaddf7a0e
ggarrett13 <g.h.garrett13@gmail.com>
2020-09-27 11:11:00 +0200

    Changed to clang (win)

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,7 +4,7 @@ if errorlevel 1 exit 1
 cd build
 if errorlevel 1 exit 1
 cmake ^
-    -G "%CMAKE_GENERATOR%" ^
+    -G "NMake Makefiles" ^
     -DCMAKE_CXX_STANDARD=14 ^
     -DBoost_NO_BOOST_CMAKE=ON ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
