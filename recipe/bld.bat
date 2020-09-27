1b936ec9788b929e3e5d9d923c721f469cd5351b
ggarrett13 <g.h.garrett13@gmail.com>
2020-09-27 10:35:07 +0200

    Update for new tudat build (win)

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,15 +4,14 @@ if errorlevel 1 exit 1
 cd build
 if errorlevel 1 exit 1
 cmake ^
-    -G "MinGW Makefiles" ^
-    -DCMAKE_CXX_STANDARD=11 ^
-    -D_hypot=hypot ^
+    -G "%CMAKE_GENERATOR%" ^
+    -DCMAKE_CXX_STANDARD=14 ^
     -DBoost_NO_BOOST_CMAKE=ON ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     ..
 if errorlevel 1 exit 1
-cmake --build . --config RelWithDebInfo --target install -- -j4 VERBOSE=1
+cmake --build . --config Release --target install
 if errorlevel 1 exit 1
-ctest
+ctest --verbose
 if errorlevel 1 exit 1
