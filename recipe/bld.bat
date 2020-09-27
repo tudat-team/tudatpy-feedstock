83c01b7f7bfa9e073a69368f4757d8ad078cdfb5
ggarrett13 <g.h.garrett13@gmail.com>
2020-09-27 12:56:05 +0200

    Attempted fix for windows and linux
    - D_ENABLE_EXTENDED_ALIGNED_STORAGE=on (win)
    - D_GLIBCXX_USE_CXX11_ABI now an option (unix)

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,9 +6,10 @@ if errorlevel 1 exit 1
 cmake ^
     -G "NMake Makefiles" ^
     -DCMAKE_CXX_STANDARD=14 ^
-    -DBoost_NO_BOOST_CMAKE=ON ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    -D_ENABLE_EXTENDED_ALIGNED_STORAGE=on ^
+    -DBoost_NO_BOOST_CMAKE=ON ^
     ..
 if errorlevel 1 exit 1
 cmake --build . --config Release --target install
