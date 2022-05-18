af76b2f4d03e23aae3e713f0fbdcfebc0655f59b
Jérémie Gaffarel <jeremie@gaffarel.pro>
2022-05-18 16:26:40 +0200

    Trying to find correct path

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
-
+echo "$PWD"
 mkdir build
 cd build
 export TUDATPY_BUILD_DIR=`pwd`
-
+echo "$PWD"
 echo "ABOUT TO BUILD TUDATPY"
 echo "NOW WOULD BE A GOOD TIME TO HAVE THE DOCSTRINGS, RIGHT?"
+
+ls /home
+ls /home/conda
+ls /home/conda/feedstock_root
+ls /home/conda/feedstock_root/build_artifacts
+ls /home/conda/feedstock_root/build_artifacts/tudatpy_1652883046229
+ls /home/conda/feedstock_root/build_artifacts/tudatpy_1652883046229/work
 
 python build_docstrings.py
 
