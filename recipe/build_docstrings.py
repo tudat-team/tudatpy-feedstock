017ba747a94055979913aa5aec70105f69834008
Jérémie Gaffarel <jeremie@gaffarel.pro>
2022-05-18 17:50:50 +0200

    Attempt importing document.py using importlib.util

--- recipe/build_docstrings.py
+++ recipe/build_docstrings.py
@@ -1,21 +1,23 @@
 print("THIS SCRIPT SHOULD BUILD THE DOCSTRINGS")
 
-import sys
+import importlib.util
 
-sys.path.insert(0, "$SRC_DIR/docs/source")
-print(sys.path)
+print("Does document.py exist? It's size is:")
+import os
+print(os.path.getsize("$SRC_DIR/docs/source/document.py"))
 
-from document import *
+spec=importlib.util.spec_from_file_location("document.","$SRC_DIR/docs/source/document.py")
+document = importlib.util.module_from_spec(spec)
 
 print("document import successful")
 
 # clone repository
-docstring_path = get_docstrings('https://github.com/tudat-team/tudat-multidoc.git')
+docstring_path = document.get_docstrings('https://github.com/tudat-team/tudat-multidoc.git')
 
 # parse api declaration
-api_declaration = parse_api_declaration(docstring_path, py=True)
+api_declaration = document.parse_api_declaration(docstring_path, py=True)
 
 # generate docstring header
-generate_docstring_header(api_declaration, "$SRC_DIR/include/tudatpy/docstrings.h")
+document.generate_docstring_header(api_declaration, "$SRC_DIR/include/tudatpy/docstrings.h")
 
 print("docstring header generated")
