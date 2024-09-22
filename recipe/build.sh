cb0a407323e243aa2a9147dc6cdb4c0cf05f4636
alfonso <alfonso.sr.trabajo@gmail.com>
2024-09-23 00:00:09 +0200

    Please YACMA

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,31 +3,29 @@
 echo "OS is:"
 echo "`uname`"
 
-python -m pip install . -v
+if [ "`uname`" = "Linux" ]
+then
+    conda install jinja2 pydantic=1.10.9 pyyaml numpydoc -y
 
-# if [ "`uname`" = "Linux" ]
-# then
-#     conda install jinja2 pydantic=1.10.9 pyyaml numpydoc -y
+    python docs/source/build_docstrings.py
+fi
+# conda install boost-cpp=1.72 -y
 
-#     python docs/source/build_docstrings.py
-# fi
-# # conda install boost-cpp=1.72 -y
+mkdir build
+cd build
+export TUDATPY_BUILD_DIR=`pwd`
 
-# mkdir build
-# cd build
-# export TUDATPY_BUILD_DIR=`pwd`
+cmake \
+    -DBoost_NO_BOOST_CMAKE=ON \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_PREFIX_PATH=$PREFIX \
+    -DCMAKE_CXX_STANDARD=14 \
+    -DTUDATPY_CONDA_BUILD=on \
+    -D_GLIBCXX_USE_CXX11_ABI=1 \
+    -DCMAKE_CXX_FLAGS_RELEASE="-Wno-macro-redefined -Wunused-parameter" \
+    ..
 
-# cmake \
-#     -DBoost_NO_BOOST_CMAKE=ON \
-#     -DCMAKE_BUILD_TYPE=Release \
-#     -DCMAKE_INSTALL_PREFIX=$PREFIX \
-#     -DCMAKE_PREFIX_PATH=$PREFIX \
-#     -DCMAKE_CXX_STANDARD=14 \
-#     -DTUDATPY_CONDA_BUILD=on \
-#     -D_GLIBCXX_USE_CXX11_ABI=1 \
-#     -DCMAKE_CXX_FLAGS_RELEASE="-Wno-macro-redefined -Wunused-parameter" \
-#     ..
+make
 
-# make
-
-# make install
+make install
