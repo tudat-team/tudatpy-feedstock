1b80797a3306ec1336ce6258444aa9997e9bfc2c
DominicDirkx <d.dirkx@tudelft.nl>
2024-10-15 14:17:43 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,12 +3,12 @@
 echo "OS is:"
 echo "`uname`"
 
-if [ "`uname`" = "Linux" ]
-then
-    conda install jinja2 pydantic=1.10.9 pyyaml numpydoc -y
+# if [ "`uname`" = "Linux" ]
+# then
+#     conda install jinja2 pydantic=1.10.9 pyyaml numpydoc -y
 
-    python docs/source/build_docstrings.py
-fi
+#     python docs/source/build_docstrings.py
+# fi
 # conda install boost-cpp=1.72 -y
 
 mkdir build
