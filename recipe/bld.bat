baadea567f4c6d2690c952c5fd495aa82c2c0359
Dominic Dirkx <d.dirkx@tudelft.nl>
2025-01-22 16:47:22 +0100

    Changing compilers

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -13,7 +13,8 @@ cmake ^
     -DCMAKE_INSTALL_PREFIX=%TUDATPY_BUILD_DIR% ^
     -DCMAKE_PREFIX_PATH=%TUDATPY_BUILD_DIR% ^
     -DCMAKE_BUILD_TYPE=Release ^
-
+    -DCMAKE_C_COMPILER=cl.exe ^
+    -DCMAKE_CXX_COMPILER=cl.exe ^
     ..
 cmake --build . --target install
 cd ../..
@@ -28,7 +29,8 @@ cmake ^
     -DCMAKE_BUILD_TYPE=Release ^
     -Dpybind11_DIR=%TUDATPY_BUILD_DIR%\share\cmake\pybind11\ ^
     -DCMAKE_CXX_FLAGS_RELEASE="-O2 -Wall -Wextra -Wno-macro-redefined -Wunused-parameter" ^
-
+    -DCMAKE_C_COMPILER=cl.exe ^
+    -DCMAKE_CXX_COMPILER=cl.exe ^
     ..
 if errorlevel 1 exit 1
 cmake --build . --verbose --config Release --target install -- VERBOSE=1
