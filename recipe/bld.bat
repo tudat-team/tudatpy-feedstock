c4cbf8a9411d20573cdacb6cdfb25eb450fc9291
Dominic Dirkx <d.dirkx@tudelft.nl>
2025-05-08 06:29:19 +0200

    Test Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -10,26 +10,28 @@ cd build
 cmake ^
     -G "NMake Makefiles" ^
     -DPYBIND11_TEST=NO ^
+    -DCMAKE_CXX_STANDARD=17 ^
     -DCMAKE_INSTALL_PREFIX=%TUDATPY_BUILD_DIR% ^
     -DCMAKE_PREFIX_PATH=%TUDATPY_BUILD_DIR% ^
     -DCMAKE_BUILD_TYPE=Release ^
-    -DCMAKE_C_COMPILER=cl.exe ^
-    -DCMAKE_CXX_COMPILER=cl.exe ^
+    -DCMAKE_C_COMPILER=clang.exe ^
+    -DCMAKE_CXX_COMPILER=clang++.exe ^
+    -DCMAKE_CXX_STANDARD=17 ^
     ..
 cmake --build . --target install
 cd ../..
 
 cmake ^
     -G "NMake Makefiles" ^
-    -DCMAKE_CXX_STANDARD=14 ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -D_ENABLE_EXTENDED_ALIGNED_STORAGE=on ^
     -DBoost_NO_BOOST_CMAKE=ON ^
     -DCMAKE_BUILD_TYPE=Release ^
     -Dpybind11_DIR=%TUDATPY_BUILD_DIR%\share\cmake\pybind11\ ^
-    -DCMAKE_C_COMPILER=cl.exe ^
-    -DCMAKE_CXX_COMPILER=cl.exe ^
+    -DCMAKE_C_COMPILER=clang.exe ^
+    -DCMAKE_CXX_COMPILER=clang++.exe ^
+    -DCMAKE_CXX_STANDARD=17 ^
     ..
 if errorlevel 1 exit 1
 cmake --build . --verbose --config Release --target install -- VERBOSE=1
